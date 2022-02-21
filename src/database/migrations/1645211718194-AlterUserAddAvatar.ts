c950f6f2c68892e870c53810710e53ddf1c3820f
Thiago Lago <thiago.lago@tips4y.pt>
2022-02-21 17:40:56 -0300

    :sparkles: + add update avatar

---
+++ src/database/migrations/1645211718194-AlterUserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterUserAddAvatar1645211718194 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
